dcddd3b4bf4e05ff444b93f181afd7c692625a2e
Duilio Palacios <duilio@styde.net>
2020-01-10 10:41:37 +0000

    Add 3 more notes in NoteSeeder

--- database/seeds/NoteSeeder.php
+++ database/seeds/NoteSeeder.php
@@ -15,13 +15,29 @@ class NoteSeeder extends Seeder
     {
         DB::table('notes')->insert([
             'title' => '¿Para qué sirve Composer?',
-            'content' => 'Con Composer podemos instalar y actualizar frameworks como Laravel o Symfony,
-                        así como componentes para generar PDF, procesar pagos con tarjetas, manipular imágenes y mucho más.',
+            'content' => '<p>Con Composer podemos instalar y actualizar frameworks como Laravel o Symfony,
+                        así como componentes para generar PDF, procesar pagos con tarjetas, manipular imágenes y mucho más.</p>',
         ]);
 
         DB::table('notes')->insert([
             'title' => 'Instalación de Laravel',
             'content' => File::get(__DIR__.'/notes/installation.html')
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Rutas y JSON',
+            'content' => File::get(__DIR__.'/notes/routes-and-json.html')
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Front Controller',
+            'content' => '<p>Front Controller es un patrón de arquitectura donde un controlador
+                        maneja todas las solicitudes o peticiones a un sitio web.</p>',
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Cambia el formato de parámetros dinámicos',
+            'content' => File::get(__DIR__.'/notes/route-parameter-constraints.html')
         ]);
     }
 }
